8e17fa12ea9ce01b74d2f74b89b50609b6d11710
James Nicholson <james.n@tencent.co.za>
2020-08-15 23:31:56 +0200

    Refactored deals - query, resolver and datasource

--- data-source.js
+++ data-source.js
@@ -9,19 +9,19 @@ class EbayAPI extends RESTDataSource{
     }
     rawDeals(data){
       const deal = (data) => {
-            return{
-                itemId: data.itemId,
-                title: data.title,
-                url: data.url,
-                endsAt: data.endsAt,
-                currency: data.currency,
-                image225: data.image225,
-                price: data.price,
-                originalPrice: data.originalPrice,
-                discountPercentage: data.discountPercentage,
-                quantity: data.quantity,
-                shippingCost:data.shippingCost,
-                dealUrl: data.dealUrl
+        return{
+            itemId: data.itemId,
+            title: data.title,
+            url: data.url,
+            endsAt: data.endsAt,
+            currency: data.currency,
+            image225: data.image225,
+            price: data.price,
+            originalPrice: data.originalPrice,
+            discountPercentage: data.discountPercentage,
+            quantity: data.quantity,
+            shippingCost:data.shippingCost,
+            dealUrl: data.dealUrl
             }
         }
         let cleaned = [];
@@ -30,16 +30,11 @@ class EbayAPI extends RESTDataSource{
         });
         return cleaned
     }
-    async dealsByCategory(category, country, limit){
-        return this.rawDeals( await this.get(`${country}?eBayCatId=${category}&limit=${limit}`))
-    }
-    async dealsByCategoryByOffset(country, category, limit, offset){
-        return this.rawDeals( await this.get(`${country}?eBayCatId=${category}&limit=${limit}&offset=${offset}`))
-    }
-    async deals(country, limit){
-        return this.rawDeals( await this.get(`${country}?limit=${limit}`))
-    }
-    async dealsByOffset(country, limit, offset){
+    async deals(args){
+        const { country, category, limit, offset } = args
+        if(category){
+            return this.rawDeals( await this.get(`${country}?eBayCatId=${category}&limit=${limit}&offset=${offset}`))
+        }
         return this.rawDeals( await this.get(`${country}?limit=${limit}&offset=${offset}`))
     }
     async countries(){
--- resolvers.js
+++ resolvers.js
@@ -1,21 +1,16 @@
+
 const Query = {
-    deals: async(_source, {country, limit}, { dataSources }) => {
-      return await dataSources.EbayAPI.deals(country, limit)
+    deals: async(_source, { country, category, limit, offset}, { dataSources }) => {
+      if(category){
+        return await dataSources.EbayAPI.deals({country, category, limit, offset})
+      }
+      return await dataSources.EbayAPI.deals({country, limit, offset})
     },
     countries: async(_source, { }, { dataSources }) => {
       return await dataSources.EbayAPI.countries()
     },
     categories: async(_source,{},{dataSources}) => {
       return await dataSources.EbayAPI.categories()
-    },
-    dealsByCategory: async(_source, { country, category, limit}, { dataSources }) => {
-      return await dataSources.EbayAPI.dealsByCategory( country, category, limit)
-    },
-    dealsByOffset: async(_source, {country, limit, offset}, { dataSources }) => {
-      return await dataSources.EbayAPI.dealsByOffset(country, limit, offset)
-    },
-    dealsByCategoryByOffset: async(_source, { country, category, limit, offset}, { dataSources }) => {
-      return await dataSources.EbayAPI.dealsByCategoryByOffset( country, category, limit, offset)
     },
   }
 module.exports = {Query}
